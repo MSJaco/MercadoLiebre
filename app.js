b82af0960329d2623eaf2ffc3ff67e5200cfc498
TorresLeandroMartin <leandromartintorres17797@gmail.com>
2023-01-02 19:42:01 -0300

    sprint2-home

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express ();
 const path = require ("path");
 const port = process.env.PORT || 3000;
 
-const publicFolderPath = path.resolve(__dirname, "./public");
+const publicFolderPath = path.resolve(__dirname, "./public1");
 app.use (express.static(publicFolderPath));
 
-app.set ("views", path.resolve(__dirname, "./views"))
+app.set ("views", path.resolve(__dirname, "./views1"))
 
 app.listen(port, () => {
     console.log("Servidor corriendo en el puerto" + " " + port);
 });
 
 app.get ("/", (req, res) => {
-    res.sendFile (path.join(__dirname, "./views/index.html"));
+    res.sendFile (path.join(__dirname, "./views1/index1.html"));
 });
 
